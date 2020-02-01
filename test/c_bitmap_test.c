ff1bc77626f07d3d8fad98138e4457d18ba7e385
frank <frankhan@gunplan.top>
2020-02-01 16:32:57 +0800

    fix-olocolor

--- test/c_bitmap_test.c
+++ test/c_bitmap_test.c
@@ -5,7 +5,7 @@
 int main(char l,char ** ls){
 	struct BitMap *b = BitMapInit(20);
 	if (!b){
-		printf("seg error\n");
+		perror("seg error\n");
 	}
 	EQUAL(AcquirePosition(b->data,6),0,"init test");
 	MakeItAs(b->data,6,(char)1);
@@ -15,8 +15,8 @@ int main(char l,char ** ls){
 	MakeItAsArea(b->data,10,15,(char)1);
 	EQUAL(AcquirePosition(b->data,12),1,"area test");
 	EQUAL(b->count,1,"size test");
-	EQUAL(b->length,1,"size test");
+	EQUAL(b->length,1,"count test");
 	EQUAL(Length(b->data),1,"size test");
-	EQUAL(Size(b->data),1,"size test");
+	EQUAL(Size(b->data),1,"count test");
 return 0;
 }
