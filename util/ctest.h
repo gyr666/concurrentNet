f31d273d7b2c34938d538eb7e6ceb2fa94d45f7b
server.auto@ <server.auto@gunplan.top>
2020-02-12 14:32:27 +0800

    move data

--- util/ctest.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void EQUAL(int val0, int val1,char *v){
-	if(val0!=val1){
-		dprintf(2,"%s\t:\033[31;1m [ERROR ] \033[0m %d not equal %d\n",v,val0,val1);
-		return;
-	}
-	dprintf(1,"%s\t:\033[32;1m [PASSED] \033[0m\n",v);
-}
-	
