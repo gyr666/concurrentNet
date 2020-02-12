97b408fdd051fcd63989c71108ed49917b008708
server.auto@ <server.auto@gunplan.top>
2020-02-12 15:15:45 +0800

    Update Buffer

--- test/stand_buffer_test.go
+++ test/stand_buffer_test.go
@@ -9,7 +9,7 @@ import (
 // TestA  Test 必须写，后边的名称随便写
 func TestStandAllocator(t *testing.T) {
 	v := buffer.NewSandBufferAllocator()
-	b := v.Alloc(30)
+	b := v.Alloc(32)
 	t.Logf("Size:%d", b.Size())
 	b.Release()
 	b = v.Alloc(30)
