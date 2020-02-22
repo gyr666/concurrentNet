09d2e8737bd8dcdd89dbe74ad1aad4a448657598
gaoyinrun <gaoyinrun@megvii.com>
2020-02-22 18:17:38 +0800

    bugfix[core/loop]: Fixed that next() could return loop with index 0

--- core/loop_group.go
+++ core/loop_group.go
@@ -9,11 +9,14 @@ func (g *subLoopGroup) registe(lp Loop){
 	g.loops=append(g.loops,lp)
 }
 
+//for put new accept connection in subLoop load balance
 func (g *subLoopGroup) next()Loop{
 	g.index ++
 	size := len(g.loops)
-	if g.index >= size{
+	if g.index == size{
 		g.index -= size
+		//index 0 is mainLoop , so we should start with index 1
+		g.index ++
 	}
 	return g.loops[g.index]
 }
